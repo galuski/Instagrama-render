81705fd1f15b22f05070e19f78e7a18047708429
Gal Luski <luskigal@gmail.com>
2024-02-19 17:46:52 +0200

    commit

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,7 @@
 export default  {
-  dbURL: 'mongodb://127.0.0.1:27017',
+  dbURL: 'mongodb+srv://theUser:thePass@mongodb+srv://luskigal:G2LLU4K1@cluster0.fgm89h8.mongodb.net/?retryWrites=true&w=majority-klgzh.mongodb.net/test?retryWrites=true&w=majority',
   dbName : 'Instagram'
 }
 
-//mongodb+srv://theUser:thePass@mongodb+srv://luskigal:G2LLU4K1@cluster0.fgm89h8.mongodb.net/?retryWrites=true&w=majority-klgzh.mongodb.net/test?retryWrites=true&w=majority
+
+//mongodb://127.0.0.1:27017
